13c18a8aaf84510fd6e59432361b00506f455cb9
terllm <907523395@qq.com>
2019-08-30 17:54:27 +0800

    1.0

---
+++ freemarker_utils/src/main/java/com/terllm/freemarker/controller/DemoController.java
@@ -0,0 +1,32 @@
+package com.terllm.freemarker.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
+
+@Controller
+public class DemoController {
+
+    @RequestMapping("demo")
+    @ResponseBody
+    public String demo(){
+
+        return "hello world !!!";
+
+    }
+
+
+    @RequestMapping("index")
+    public String index(Map<String,Object> map){
+
+        map.put("name","hello world !!!");
+
+        return "index";
+
+    }
+
+
+
+}
